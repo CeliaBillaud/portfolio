3b7f47b81043ea2cd99d189dfd102c99e429f193
CeliaBillaud <celiabillaud2@gmail.com>
2024-09-12 12:22:37 +0200

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Anek_Telugu } from 'next/font/google';
 import "./globals.css";
 
 const geistSans = localFont({
@@ -11,6 +12,11 @@ const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
+});
+
+const anekTelugu = Anek_Telugu({
+  subsets: ['latin'],
+  variable: '--font-caption',
 });
 
 export const metadata: Metadata = {
