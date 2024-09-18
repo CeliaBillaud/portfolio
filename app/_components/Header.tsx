f2f9114d1e9639a8855ebb306aeadf34cb47ffae
CeliaBillaud <celiabillaud2@gmail.com>
2024-09-18 13:22:45 +0200

    nav github link

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,23 +1,30 @@
-import { Button } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import { Section } from "./Section"
 import { GithubIcon } from "./icons/GithubIcon"
+import { Link } from "lucide-react"
+import clsx from "clsx"
 
 export const Header = () => {
     return(
-        <header className="sticky top-0">
-            <Section className="flex items-baseline">
-                <p className="text-4xl font-bold text-primary">
+        <header className="sticky top-0 py-4">
+            <Section className="flex items-baseline place-content-between">
+                <p className="text-lg font-bold text-primary">
                     Hello !
                 </p>
                 {/* todo : changeer alignement */}
-                <nav className="flex-1">
+                <div className="flex items-end">
                 {/* copy components from shadcn/ui */}
-                    <ul>
-                        <Button className="size-6svg">
-                            <GithubIcon size={16} className="text-foreground"/>
-                        </Button>
-                    </ul>
-                </nav>
+                    <nav>
+                        <ul className="flex items-center gap-2">
+                            <a 
+                            href="https://github.com/CeliaBillaud"
+                            className="size-6 p-0 flex items-center justify-center hover:opacity-80 transition-opacity border border-transparent rounded-md hover:border-slate-500 " >
+
+                                <GithubIcon size={16} className="text-foreground"/>
+                            </a>
+                        </ul>
+                    </nav>
+                </div>
             </Section>
         </header>
     )
