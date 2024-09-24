4359c4f84156523c7708750c6772ca4a3e0d2e03
CeliaBillaud <celiabillaud2@gmail.com>
2024-09-24 13:27:36 +0200

    avatar added, profile pic supp

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -3,17 +3,24 @@ import { Section } from "./Section"
 import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons"
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 
 export const Header = () => {
     return(
         <header className="sticky top-0 py-4">
-            <Section className="flex items-baseline place-content-between">
-                <Link 
-                    href="/"
-                    className="text-lg font-bold text-foreground">
-                    Hello !
-                </Link>
-                {/* todo : changeer alignement */}
+            <Section className="flex items-center place-content-between">
+                <div className='flex items-center gap-2'>
+                    <Avatar>
+                        <AvatarImage src="/profile-pic(8).png" />
+                        <AvatarFallback>CB</AvatarFallback>
+                    </Avatar>
+                    <Link 
+                        href="/"
+                        className="text-lg font-bold text-foreground">
+                        Hello !
+                    </Link>
+                </div>
                 <div className="flex items-end">
                 {/* copy components from shadcn/ui */}
                     <nav>
--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -3,23 +3,12 @@ import Image from "next/image"
 
 export const Hero = () => {
     return (
-        <Section className="flex justify-center max-md:flex-col items-center content-center">
-            <div className="flex-[2]">
-                <h1 className="text-5xl font-bold my-2 font-caption">Célia Billaud • Développeuse web</h1>
-                <h2 className="text-xl mb-4">Votre future alternante ?</h2>
-                <p className="text-base">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus, totam voluptas libero modi alias. Dolores illo voluptas modi enim nemo fugiat debitis, minus necessitatibus sapiente quo facilis dicta doloremque?
-                </p>
-            </div>
-            <div className="flex-1 w-full max-w-[200px] md:max-w-[250px] lg:max-w-[300px] mt-6 lg:mt-0 flex justify-center content-center">
-                <Image
-                    src="/profile-pic(8).png"
-                    alt="Photo de Célia Billaud"
-                    width={300}
-                    height={300}
-                    className="w-full h-auto object-cover"
-                />
-            </div>
+        <Section >
+            <h1 className="text-5xl font-bold my-2 font-caption">Célia Billaud • Développeuse web</h1>
+            <h2 className="text-xl mb-4">Votre future alternante ?</h2>
+            <p className="text-base">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus, totam voluptas libero modi alias. Dolores illo voluptas modi enim nemo fugiat debitis, minus necessitatibus sapiente quo facilis dicta doloremque?
+            </p>
         </Section>
     )
 }
