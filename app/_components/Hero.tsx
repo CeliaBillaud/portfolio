ed6070d69e1c823c60293b40389666d2f4ed6239
CeliaBillaud <celiabillaud2@gmail.com>
2024-09-27 13:42:02 +0200

    oclock added to description

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -4,10 +4,12 @@ import { Section } from "./Section";
 import { cn } from "@/lib/utils";
 import { FaPhp, FaReact } from 'react-icons/fa';
 import { SiSymfony, SiNextdotjs } from 'react-icons/si';
+import Link from 'next/link';
+import Image from 'next/image'
 
 const Code = ({ className, children, icon: Icon, ...props }: ComponentPropsWithRef<"span"> & { icon?: React.ElementType }) => {
     return (
-        <span className={cn("font-mono bg-accent/30 border border-accent p-1 m-1 rounded-sm text-primary inline-flex items-center", className)} {...props}>
+        <span className={cn("font-mono bg-accent/30 border hover:bg-accent/50 transition-colors border-accent px-1 py-0.5 m-1 rounded-sm text-primary inline-flex items-center", className)} {...props}>
             {Icon && <Icon className="mr-1" />}
             {children}
         </span>
@@ -19,18 +21,27 @@ export const Hero = () => {
         <Section>
             <h1 className="text-5xl text-primary font-bold font-caption">Célia Billaud • Développeuse web</h1>
             <h2 className="text-3xl font-caption">Votre future alternante ?</h2>
-            <p>
-                J'aime créer des projets en <Code icon={FaPhp}>PHP</Code>/<Code icon={SiSymfony}>Symfony</Code> et je me forme au monde merveilleux de <Code icon={FaReact}>React</Code> et <Code icon={SiNextdotjs}>Next.js</Code>
+            <p className="text-base md:text-justify">
+                J'aime créer des projets en <Code icon={FaPhp}>PHP</Code><Code icon={SiSymfony}>Symfony</Code> et je me forme au monde merveilleux de <Code icon={FaReact}>React</Code> et <Code icon={SiNextdotjs}>Next.js</Code>
             </p>
-            <br/>
-            <p>
-                Ancienne étudiante en Finance Internationale et vidéaste, je me suis lancée dans le développement web, attirée par la résolution de problèmes et la création.
+            <p className="text-base md:text-justify">
+                Ancienne étudiante en Finance Internationale et vidéaste, attirée par la résolution de problèmes et la création, je me suis lancée dans le développement web avec l'
+                <Link href="https://oclock.io/" target="_blank">
+                    <Code> 
+                            <Image 
+                            src="/logooclock.png" 
+                            width = {20}
+                            height = {20}
+                            alt="logo O'clock" 
+                            className="mr-1"/>
+                    École O'clock
+                    </Code>.
+                </Link>
             </p>
-            <br/>
-            <p>
+            <p className="text-base md:text-justify">
                 Je recherche une <strong>alternance de 20 mois</strong> pour devenir Conceptrice Développeuse d'Applications.
             </p>
-            <p>
+            <p className="text-base md:text-justify">
                 Passionnée, créative, toujours en quête d'apprentissage !
             </p>
         </Section>
