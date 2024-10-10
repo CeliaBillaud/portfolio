4e37d7e233c3048df90fbd79ea8dd30e108d39a4
CeliaBillaud <celiabillaud2@gmail.com>
2024-10-10 17:13:18 +0200

    Mon parcours added

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 import { FaPhp, FaReact } from 'react-icons/fa';
 import { SiSymfony, SiNextdotjs } from 'react-icons/si';
 import Link from 'next/link';
-import Image from "next/image"
+import Image from "next/image";
 
 const Code = ({ className, children, icon: Icon, ...props }: ComponentPropsWithRef<"span"> & { icon?: React.ElementType }) => {
     return (
--- app/_components/Status.tsx
+++ app/_components/Status.tsx
@@ -6,6 +6,7 @@ import { IconType } from 'react-icons'
 import { FaPhp, FaBootstrap, FaReact, FaCss3Alt } from 'react-icons/fa'
 import { SiSymfony, SiMysql, SiSass, SiJavascript, SiTypescript, SiTailwindcss } from 'react-icons/si'
 import { RiNextjsFill } from "react-icons/ri";
+import Image from 'next/image';
 
 
 export const Status = () => {
@@ -28,7 +29,19 @@ export const Status = () => {
         <div className="flex-[2] flex flex-col gap-4 w-full lg:w-1/2">
             <Card className="p-4 flex-1 w-full">
                 <p className="text-lg text-muted-foreground">Mon parcours</p>
-                <p className="text-sm text-muted-foreground">Ancienne étudiante en Finance Internationale et vidéaste, attirée par la résolution de problèmes et la création, je me suis lancée dans le développement web avec l'École O'clock.</p>
+                <div className="flex flex-col gap-4">
+                {EXPERIENCE.map((experience, index) => 
+                <Experience 
+                    key={index}
+                    image={experience.image}
+                    imageAlt= {experience.imageAlt}
+                    title= {experience.title}
+                    description= {experience.description}
+                    url= {experience.url}
+                    date= {experience.date}
+                />
+                )}
+                </div>
             </Card>
             <Card className="p-4 flex-1 w-full">
                 <p className="text-lg text-muted-foreground">Me contacter</p>
@@ -45,6 +58,13 @@ const PROJECT = [
         description: "O’MyBeer est une application dédiée à l'univers de la bière, qui permet aux utilisateurs d’explorer une vaste sélection de bières et de marques du monde entier.", 
         url: "https://www.omybeer.ovh/",
         techno: [SiSass, FaBootstrap, SiJavascript, FaPhp, SiSymfony, SiMysql]
+    },
+    {
+        Logo: Beer,
+        title: "MaReco",
+        description: "MaReco permet de partager ses recommendations culturelles toutes les semaines à ses amis. En cours de développement...", 
+        url: "/",
+        techno: [SiTailwindcss, FaReact, FaPhp, SiSymfony, SiMysql]
     },
     {
         Logo: BriefcaseBusiness,
@@ -74,7 +94,7 @@ const iconNames = {
     FaBootstrap: "Bootstrap",
     FaCss3Alt: "CSS",
     RiNextjsFill: "Next.js"
-}
+};
 
 type ProjetProps = {
     Logo: LucideIcon;
@@ -105,6 +125,58 @@ const Project = (props :
                                 );
                 })
                         }
+                    </div>
+                </Link>
+                <p className="text-sm text-muted-foreground p-1">{props.description}</p>
+            </div>
+};
+
+const EXPERIENCE = [
+    {   
+        image:"/logooclock.png",
+        imageAlt: "logo O'clock",
+        title: "Formation O'clock",
+        description: "lorem ipsum dolor sit amet consectetur adipisicing",
+        url: "oclock.io",
+        date: "Oct 2023 - Juin 2024", 
+    },
+    {   
+        image:"/logoiae.png",
+        imageAlt: "logo IAE Aix",
+        title: "Msc International Finance",
+        description: " Formation BAC+5 en finance spécialisée en finance internationale",
+        url: "oclock.io",
+        date: "2020", 
+    },
+]
+
+type ExperienceProps = {
+    image: string;
+    imageAlt: string;
+    title: string;
+    description: string;
+    url: string;
+    date: string;
+};
+
+const Experience = (props :
+    ExperienceProps) => {
+    return <div className="flex-col gap-4">
+                <Link href={props.url} className="flex items-center  hover:bg-accent/50 transition-color p-1 justify-between" target="alt">
+                    <div className="flex items-center gap-4">
+                        <span className="rounded-sm">
+                            <Image 
+                                src={props.image} 
+                                width = "30"
+                                height = "30"
+                                alt={props.imageAlt}
+                                />
+                        </span>
+                        <div className="flex flex-col">
+                            <p className="text-lg font-semibold">{props.title}</p>
+                            <p className="text-sm text-muted-foreground">{props.date}</p>
+                            
+                        </div>
                     </div>
                 </Link>
                 <p className="text-sm text-muted-foreground p-1">{props.description}</p>
