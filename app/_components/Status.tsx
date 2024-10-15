aae1203833bd507046d8c5f2d7d0c3929d5f7b88
CeliaBillaud <celiabillaud2@gmail.com>
2024-10-15 17:50:20 +0200

    contact card wip

--- app/_components/Status.tsx
+++ app/_components/Status.tsx
@@ -1,6 +1,6 @@
 import { Card } from "@/components/ui/card"
 import { Section } from "./Section"
-import {Home, LucideIcon, Beer, BriefcaseBusiness, Trophy, MessageSquareHeart} from "lucide-react"
+import {Home, LucideIcon, Beer, BriefcaseBusiness, Trophy, MessageSquareHeart, ArrowUpRight} from "lucide-react"
 import Link from "next/link"
 import { IconType } from 'react-icons'
 import { FaPhp, FaBootstrap, FaReact, FaCss3Alt } from 'react-icons/fa'
@@ -11,7 +11,7 @@ import Image from 'next/image';
 
 export const Status = () => {
     return <Section className="flex max-sm:flex-col items-start gap-4">
-        <Card className="flex-[3] p-4 flex flex-col gap-2 w-full lg:w-1/2">
+        <Card className="p-4 flex flex-col gap-2 w-full lg:w-1/2">
             <p className="text-lg text-muted-foreground">Mes projets préférés</p>
             <div className="flex flex-col gap-4">
                 {PROJECTS.map((project, index) => 
@@ -26,7 +26,7 @@ export const Status = () => {
                 )}
             </div>    
         </Card>
-        <div className="flex-[2] flex flex-col gap-4 w-full lg:w-1/2">
+        <div className="flex flex-col gap-4 w-full lg:w-1/2">
             <Card className="p-4 flex-1 w-full">
                 <p className="text-lg text-muted-foreground">Mon parcours</p>
                 <div className="flex flex-col gap-4">
@@ -47,11 +47,50 @@ export const Status = () => {
             </Card>
             <Card className="p-4 flex-1 w-full">
                 <p className="text-lg text-muted-foreground">Me contacter</p>
+                <ContactCard 
+                    name="Célia Billaud"
+                    image="/profile-pic(8).png"
+                    mediumImage="https://img.freepik.com/vecteurs-premium/linkedin-logo_578229-227.jpg"
+                    description="Envoyez-moi un message !"
+                    url="https://www.linkedin.com/in/celiabillaud/"
+                />
+                <ContactCard 
+                    name="Célia Billaud"
+                    image="/profile-pic(8).png"
+                    mediumImage=""
+                    description="Envoyez-moi un mail !"
+                    url="mailto:celiabillaud.dev@gmail.com"
+                />
+
             </Card>
 
         </div>
     </Section>
 };
+
+ const ContactCard = (props:{
+    url: string;
+    image: string;
+    mediumImage: string;
+    name: string;
+    description: string;
+    }) => {
+        return(
+            <Card className="p-3 bg-accent/10 flex gap-4">
+                    <div className="relative">
+                        <img src={props.image} alt={props.name}  className="w-10 h-10"/>
+                        <img src={props.mediumImage} alt={props.name}  className="w-4 h-4 absolute -bottom-1 -right-1 rounded-sm"/>
+                    </div>
+                    <div className="flex flex-col">
+                        <p className="text-lg font-semibold">{props.name}</p>
+                        <p className="text-sm text-muted-foreground">{props.description}</p>
+                    </div>
+                    <Link href={props.url} target="alt">
+                        <ArrowUpRight size={16}/>
+                    </Link>
+            </Card>
+        )
+    }
 
 const PROJECTS: ProjectProps[] = [
     {
@@ -71,14 +110,14 @@ const PROJECTS: ProjectProps[] = [
     {
         Logo: BriefcaseBusiness,
         title: "Mon Portfolio",
-        description: "lorem ipsum dolor sit amet consectetur adipisicing", 
+        description: "Ce projet de portfolio m'a permis de découvrir et m'amuser avec Next.js, React, Typescript et Tailwind CSS !", 
         url: "/",
         techno: [RiNextjsFill, FaReact, SiTypescript, SiTailwindcss ]
     },
     {
         Logo: Trophy,
         title: "Rubgy Quizz",
-        description: "lorem ipsum dolor sit amet consectetur adipisicing", 
+        description: "Un quizz sur les joueurs du XV de France, je m'étais mis le challenge de le réaliser en 2 jours avec seulement des technos de base.", 
         url: "https://celiabillaud.github.io/rugby-quizz/",
         techno: [FaCss3Alt, SiJavascript]
     }
@@ -116,7 +155,21 @@ const Project = (props :
                         </span>
                         <p className="text-lg font-semibold">{props.title}</p>
                     </div>
-                    <div className="flex gap-2">
+                    {/* <div className="flex gap-2">
+                        {props.techno.map((Icon, index) => {
+                            //todo add title on hover
+                            return (
+                                <Icon 
+                                    key={index} 
+                                    className="text-muted-foreground size-6"
+                                    role="img" />
+                                );
+                            })
+                        }
+                    </div> */}
+                </Link>
+                <p className="text-sm text-muted-foreground p-1">{props.description}</p>
+                <div className="flex gap-2 justify-end">
                         {props.techno.map((Icon, index) => {
                             //todo add title on hover
                             return (
@@ -128,8 +181,6 @@ const Project = (props :
                             })
                         }
                     </div>
-                </Link>
-                <p className="text-sm text-muted-foreground p-1">{props.description}</p>
             </div>
 };
 
